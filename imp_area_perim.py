4fdb1116fd57c8e3f9f9643815dbd583bfd74267
sailakshmi <zylaxmi.sk@gmail.com>
2021-03-30 21:27:58 +0530

    py

---
+++ imp_area_perim.py
@@ -0,0 +1,12 @@
+from area import circle as c
+from area import rectangle as r
+from area.sub_pack import cuboid as cu
+from area.sub_pack import sphere as s
+print(c.area(2))
+print(c.peri(2))
+print(r.area(2,2))
+print(r.perim(2,2))
+print(cu.area(2,2,2))
+print(cu.perim(2,2,2))
+print(s.area(2))
+print(s.perim(2))
